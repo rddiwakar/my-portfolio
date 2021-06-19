bd8282f7c768a49015e4ebe397b9da14ef83d790
rddiwakar <rahuldiwakar611@gmail.com>
2021-06-19 14:34:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let projects = document.getElementById("projects");
+let projectArr = [
+    {
+        name: "technical-documentary-page",
+        img:"./technical.png" 
+    },
+    {
+        name: "randomcatgenerator",
+        img:"./randomcat.png"
+    },
+    {
+        name: "Rockpaperscissor",
+        img:"./rock-papaer-scissor.png"  
+    },
+    {
+        name: "todo-list",
+        img:"./todo.png"   
+    },
+    {
+        name: "product-landing-page",
+        img:"./landing.png"  
+    }
+]
+
+projects.innerHTML = `<h1>These are some of my projects.</h1>
+    <hr/>
+    ${String(projectArr.map(project =>{
+        return `<li> <img src=${project.img}/>
+                    <a href="https://rddiwakar.github.io/${project.name}/">
+                        <h2>${project.name} </h2>
+                    </a>
+               </li>`
+    })).replace(/,/g,"")}
+    <button><a href="https://github.com/rddiwakar?tab=repositories" target="_blank">Show All</a></button>
+`
+
