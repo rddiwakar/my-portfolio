7ca6d99bd70c8bdfb7b774e219173d7067518f06
rddiwakar <rahuldiwakar611@gmail.com>
2021-06-21 12:04:31 +0530

    making responsive

--- index.js
+++ index.js
@@ -24,13 +24,15 @@ let projectArr = [
 
 projects.innerHTML = `<h1>These are some of my projects.</h1>
     <hr/>
-    ${String(projectArr.map(project =>{
-        return `<li> <img src=${project.img}/>
-                    <a href="https://rddiwakar.github.io/${project.name}/">
-                        <h2>${project.name} </h2>
-                    </a>
-               </li>`
-    })).replace(/,/g,"")}
+    <ul>
+        ${String(projectArr.map(project =>{
+            return `<li> <img src=${project.img} />
+                        <a href="https://rddiwakar.github.io/${project.name}/">
+                            <h2>${project.name} </h2>
+                        </a>
+                </li>`
+        })).replace(/,/g,"")}
+    </ul>
     <button><a href="https://github.com/rddiwakar?tab=repositories" target="_blank">Show All</a></button>
 `
 
